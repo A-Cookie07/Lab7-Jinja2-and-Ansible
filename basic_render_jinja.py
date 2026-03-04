9566196e3bc9c5baa91c632b620f3ff97b0b2a91
netman <zach0311@comcast.net>
2026-03-04 11:39:40 -0700

    Got jinja 2 router template created and set up roles directory structure

---
+++ basic_render_jinja.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import jinja2
+
+def main():
+    test_item = {
+        'address00': "10.0.0.1 255.255.255.0",
+        'address10': "10.0.1.1 255.255.255.0",
+        'address20': "10.0.2.1 255.255.255.0",
+        'addresslo': "25.25.25.25 255.255.255.255"
+    }
+
+    print("This is working!")
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "router_template.j2"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+    print(template.render(item=test_item))
+
+if __name__ == "__main__":
+    main()
