38f4e2ae00610be549552c27ca01878dc39e1485
netman <zach0311@comcast.net>
2026-03-09 18:04:21 -0600

    Got the Config Done

--- configure_hosts.py
+++ configure_hosts.py
@@ -2,12 +2,14 @@
 from netmiko import ConnectHandler
 
 def configure_router(hostname, ip):
+    username = input(f"Enter username for {hostname}:\n>")
+    secret = input(f"Enter secret for {hostname}:\n>")
     device0 = {
                 "device_type": "cisco_ios",
                 "host": ip,
-                "username": 'zaco6003',
-                "password": 'cisco',
-                "secret": 'cisco',
+                "username": username,
+                "password": secret,
+                "secret": secret,
             }
 
     with ConnectHandler(**device0) as connection:
@@ -18,11 +20,11 @@ def configure_router(hostname, ip):
         output = connection.send_config_from_file(f"{hostname}.txt")
         print(output)
 
-    
-
 def main():
     #print("This is working!")
     configure_router("R3", '198.51.100.5')
+    configure_router("R2", '198.51.100.4')
+    configure_router("R1", '198.51.100.3')
 
 if __name__ == "__main__":
     main()
